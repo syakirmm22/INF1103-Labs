af6a3540e95ca17876a213b6616da9c91fd6871e
Syakir <2603220@singaporetech.edu.sg>
2026-09-04 17:12:12 +0800

    Activity 2

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -7,6 +7,17 @@ username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
 
+
 print("username:", username)
 print("bio:", bio)
 print("followers:", followers)
+
+followers = 100
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
