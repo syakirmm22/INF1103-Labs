56de3013fff8e076c7b6666e9503e03727d55f3c
Syakir <2603220@singaporetech.edu.sg>
2026-09-04 17:22:55 +0800

    Activity 4

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -24,3 +24,13 @@ print("Day 3:", followers)
 
 followers -= 10
 print("Day 4:", followers)
+
+username = input("Enter username: ")
+age = input("Enter age: ")
+category = input("Enter content category: ")
+
+print("\nInstagram Profile")
+print("============")
+print("Username:", username)
+print("Age:", age)
+print("Content Category:", category)
