34172d60eb5c9d3434c8007d7be7282e98c60ad2
Syakir <2603220@singaporetech.edu.sg>
2026-09-04 17:03:47 +0800

    Activity 2

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -2,3 +2,11 @@ print("=================================================")
 print("welcome here")
 print("my first post!")
 print("=================================================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("username:", username)
+print("bio:", bio)
+print("followers:", followers)
