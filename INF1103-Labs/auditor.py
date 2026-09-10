38025193c83fdc9a663c5d89f8fbc7903a457be7
Syakir <2603220@singaporetech.edu.sg>
2026-09-10 22:58:13 +0800

    Part 2

--- INF1103-Labs/auditor.py
+++ INF1103-Labs/auditor.py
@@ -3,18 +3,24 @@ failed_entries = 0
 
 stock = (input("Enter the stock inventory: "))
 
-if stock.isdigit():
-    stock_int = int(stock)
-    total_inventory += stock_int
-    print("Total inventory:", total_inventory)
+while total_inventory < 500:
 
-elif int(stock) < 0:
-    print("Invalid entry. Stock cannot be negative.")
-    failed_entries += 1
+    stock = input("Enter the stock inventory or enter 'QUIT' to exit: ")
 
-else: 
-    print("Invalid entry. Please enter a whole number and not in word form.")
-    failed_entries += 1        
+    if stock == 'QUIT':
+        break
+
+    if stock.isdigit():
+        stock_int = int(stock)
+        total_inventory += stock_int
+        print("Total inventory:", total_inventory)
+
+    elif int(stock) < 0:
+        print("Invalid entry. Stock cannot be negative.")
+        failed_entries += 1
+    else: 
+        print("Invalid entry. Please enter a whole number and not in word form.")
+        failed_entries += 1        
 
 
 
