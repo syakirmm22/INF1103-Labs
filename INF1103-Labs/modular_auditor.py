e34c16ba37cc3a483e9917a2a4ad82e5ffaead8c
Syakir <2603220@singaporetech.edu.sg>
2026-09-21 11:20:02 +0800

    Activity 2

---
+++ INF1103-Labs/modular_auditor.py
@@ -0,0 +1,52 @@
+#get_valid_input(): Handles the prompt, handles input validation, 
+# and returns a valid integer or a "quit" signal.
+
+#process_delivery(current_total, new_value): Calculates the new total and returns it.
+
+#calculate_tax(amount): A new requirement! This function takes a delivery 
+# amount and returns the tax (10% of that specific delivery).
+
+#generate_report(total_units, failed_attempts): A dedicated function to print the final summary.
+
+
+def get_valid_input(): 
+    user_input = input("Enter a valid integer or 'QUIT' to exit: ")
+    if user_input == 'QUIT':
+        return "QUIT"
+    if user_input.isdigit():
+        return int(user_input)
+    else:
+        print("Invalid input. Please enter a whole number.")
+        return None
+
+
+
+
+
+
+
+total_inventory = 0
+failed_entries = 0
+
+while total_inventory <= 500:
+
+    stock = input("Enter the stock inventory or enter 'QUIT' to exit: ")
+
+    if stock == 'QUIT':
+        break
+
+    if stock.isdigit():
+        stock_int = int(stock)
+        total_inventory += stock_int
+        print("Total inventory:", total_inventory)
+
+    else: 
+        print("Negative value/Invalid entry. Please enter a whole number and not in word form.")
+        failed_entries += 1   
+
+
+    if total_inventory > 500:
+        print("Inventory limit reached. Cannot add more stock.")
+        break
+        
+print("Total Deliveries Processed:", total_inventory, "Total Failed Entries:", failed_entries)  
