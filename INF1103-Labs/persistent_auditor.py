f9dd2fe34cbc49e322236dce31e220a1ee727050
Syakir <2603220@singaporetech.edu.sg>
2026-09-25 17:24:33 +0800

    test

--- INF1103-Labs/persistent_auditor.py
+++ INF1103-Labs/persistent_auditor.py
@@ -1,4 +1,5 @@
-
+file_name = "inventory.txt"
+orders = [10001, "Wireless Mouse", 2], [10002, "Keyboard", 1], [10003, "USB Cable", 3]  #this is the initial order
      
 
 
@@ -11,7 +12,10 @@ def read_orders():
         return [] #Return an empty list if the file doesnt exist
 
 def write_orders():
-    try:        
+    with open(file_name, "w") as file:
+        file.writelines(orders)
+        return orders      
+        
 
 
 
